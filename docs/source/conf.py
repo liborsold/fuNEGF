9e88cee2eef18f2cb793823525ba57d9c2edd6df
Libor Vojáček <vojaceklibor@gmail.com>
2024-09-24 09:18:43 +0200

    improve documentation website

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
-    "repository_url": "https://github.com/liborsold/spinWannier",
+    "repository_url": "https://github.com/liborsold/fuNEGF",
     "use_source_button": False,
     "use_repository_button": True,
     "repository_branch": "master",
